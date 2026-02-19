f3d674d613923a3cb71b34488b8ac6ce029a86e1
iamyogurthh <saieainkhant992003@gmail.com>
2026-02-19 22:20:02 +0630

    week 4 stated

--- resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.default')
-
-@section('content')
-<h2>About Page</h2>
-@foreach ($data as $key => $value)
-<div>{{ $key . ' = '. $value }}</div>
-@endforeach
-@endsection
--- resources/views/contact.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.default')
-
-@section('content')
-<h2>Contact Page</h2>
-@foreach ($data as $key => $value)
-<div>{{ $key . ' = '. $value }}</div>
-@endforeach
-@endsection
